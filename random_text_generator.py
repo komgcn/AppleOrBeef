e026a9db1b7d7268703e4301ceae40ebbe76706b
komgcn <gary_komgcn@hotmail.com>
2018-07-12 11:10:50 +0800

    configuring training part

--- random_text_generator.py
+++ random_text_generator.py
@@ -1,26 +1,38 @@
 import random
 
-#Generate a .txt file that contains 10,000 lines, made up of random number of words,
-#words are chosen randomly from the list of words
+# Generate a .txt file that contains lines of random number of words
+# Words are chosen "randomly" from the list of words(80% as keywords, 20% as other words)
 
-apple_words = ['apple', 'orange','banana','pear','cherry','i','have','eat','like','some','today','school']
+# Number of lines
+num_lines = 10000;
 
-beef_words = ['fish', 'beef','lamb','chicken','duck','i','have','eat','like','some','today','school']
+# Maximum number of words in a line
+num_words = 20;
 
+# Proportion of keywords and others
+key = 7
+
+apple_words = ['apple', 'orange','banana','pear','cherry']
+
+beef_words = ['fish', 'beef','lamb','chicken','duck']
+
+other_words = ['i','have','eat','like','some','today','school']
 
 def generate(text):
     with open(text,'w') as f:
-        for _ in range(10000):
+        for _ in range(num_lines):
             sentence = []
-            for _ in range(random.randint(1,20)):
-                if text == 'apple.txt':
-                    sentence.append(apple_words[random.randint(0,11)])
-                elif text == 'beef.txt':
-                    sentence.append(beef_words[random.randint(0, 11)])
+            for _ in range(random.randint(1,num_words)):
+                if random.randint(0,10) < key :
+                    if text == 'apple.txt':
+                        sentence.append(apple_words[random.randint(0,len(apple_words)-1)])
+                    elif text == 'beef.txt':
+                        sentence.append(beef_words[random.randint(0, len(beef_words)-1)])
+                else:
+                    sentence.append(other_words[random.randint(0,len(other_words)-1)])
             sentence = ' '.join(sentence)
             print(sentence)
             f.write(sentence+'\n')
-
 
 #generate('apple.txt')
 #generate('beef.txt')
